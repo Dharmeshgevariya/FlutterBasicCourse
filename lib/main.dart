4835f719b484c93ce6e7b873b52aa48d4a8b38aa
Dharmesh <gevariya.d>
2022-11-23 14:47:42 +0530

    SingleChildScrollView & screen forword

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catelog/pages/login_page.dart';
+import 'package:flutter_catelog/utils/routs.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
@@ -17,11 +18,11 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.deepPurple,
           fontFamily: GoogleFonts.kanit().fontFamily),
       themeMode: ThemeMode.light,
-      initialRoute: "/login",
+      initialRoute: MyRoutes.loginRoutes,
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        MyRoutes.homeRoutes: (context) => HomePage(),
+        MyRoutes.loginRoutes: (context) => LoginPage()
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,52 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catelog/utils/routs.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/image/P1.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20.0,
-              width: 100.0,
-            ),
-            Text("Welcome", style: TextStyle(fontSize: 40)),
-            SizedBox(
-              height: 20.0,
-              width: 100.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(28),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                        hintText: "Enter Mobile Number",
-                        labelText: "MobileNumber"),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: "Enter Password ", labelText: "Password"),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                    width: 100.0,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        print("hi welcome to button onPress");
-                      },
-                      child: Text("Login"))
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/image/P1.png",
+                height: 300.0,
+                fit: BoxFit.cover,
               ),
-            )
-          ],
+              SizedBox(
+                height: 20.0,
+                width: 100.0,
+              ),
+              Text("Welcome", style: TextStyle(fontSize: 40)),
+              SizedBox(
+                height: 20.0,
+                width: 100.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(28),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Enter Mobile Number",
+                          labelText: "MobileNumber"),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: "Enter Password ", labelText: "Password"),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                      width: 100.0,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, MyRoutes.homeRoutes);
+                        },
+                        child: Text("Login"),
+                        style:
+                            TextButton.styleFrom(minimumSize: Size(420, 40))),
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
