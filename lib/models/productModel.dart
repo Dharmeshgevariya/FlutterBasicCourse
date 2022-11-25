2973cf1206546c346f32653857373477b82e4ff5
Dharmesh <gevariya.d>
2022-11-25 09:46:18 +0530

    model design

---
+++ lib/models/productModel.dart
@@ -0,0 +1,24 @@
+class Item {
+  final num id;
+  final String name;
+  final String cat;
+  final num price;
+  final String image;
+  final num qty;
+  final String unit;
+
+  Item(this.id, this.name, this.cat, this.price, this.image, this.qty,
+      this.unit);
+}
+
+final products = [
+  Item(
+      id: 00120,
+      name: "Balaji Wafers",
+      cat: "snakes",
+      price: 45,
+      image:
+          "https://www.balajiwafers.com/wp-content/uploads/2019/04/T_Wafers_SimplySalted_Middle.png",
+      qty: 45,
+      unit: "PCS")
+];
