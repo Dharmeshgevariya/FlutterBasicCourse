f7306488879cd230de63ed90fc716fa16a030a40
Dharmesh <gevariya.d>
2022-11-29 20:03:11 +0530

    Using Cubits | BLoC Explanation and Usage

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -2,28 +2,25 @@ import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_catelog/blocs/internet_blocs/internet_event.dart';
-import 'package:flutter_catelog/blocs/internet_blocs/internet_state.dart';
+import 'package:flutter_catelog/cubit/intenet_cubit.dart';
 import 'package:flutter_catelog/widgets/ProductAppBar.dart';
-
-import '../blocs/internet_blocs/internet_blocs.dart';
 
 class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => InternetBloc(),
+      create: (context) => InternetCubit(),
       child: Scaffold(
           body: SafeArea(
               child: Center(
-        child: BlocConsumer<InternetBloc, InternetState>(
+        child: BlocConsumer<InternetCubit, InternetState>(
           listener: (context, state) {
-            if (state is InternetGainedState) {
+            if (state == InternetState.Gained) {
               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                 content: Text("Internet Connected!!!"),
                 backgroundColor: Colors.green,
               ));
-            } else if (state is InternetLostState) {
+            } else if (state == InternetState.Lost) {
               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                 content: Text("Internet Dis-Connected!!!"),
                 backgroundColor: Colors.red,
@@ -31,9 +28,9 @@ class ProductPage extends StatelessWidget {
             }
           },
           builder: ((context, state) {
-            if (state is InternetGainedState) {
+            if (state == InternetState.Gained) {
               return Text("Connected ");
-            } else if (state is InternetLostState) {
+            } else if (state == InternetState.Lost) {
               return Text("Not Conneted");
             } else {
               return Text("Loadding!!!!!..");
